03e013bbcb01a3bd979e7856f26cf206991e4eb7
lliqi <614124284@qq.com>
2020-06-05 11:54:53 +0800

    安装html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,28 @@
 const path = require("path");
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: "./src/app.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "app.js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
 };
