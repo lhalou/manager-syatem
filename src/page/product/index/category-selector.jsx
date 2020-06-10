4bf7b8978965aa62c7177f6f2dd93e852ce067b7
lliqi <614124284@qq.com>
2020-06-10 20:37:26 +0800

    安装react-fileupload

---
+++ src/page/product/index/category-selector.jsx
@@ -0,0 +1,110 @@
+import React,{Component} from "react"
+import "./category-selector.scss"
+import Product from "service/product-service.jsx"
+import Util from "util/mm.jsx"
+const _mm = new Util()
+const _product = new Product()
+class CategorySelector extends Component {
+  constructor(props){
+    super(props),
+    this.state = {
+      firstCategoryList: [],
+      firstCategoryId: 0,
+      secondCategoryList: [],
+      secondCategoryId: 0
+    }
+  }
+  componentDidMount(){
+    this.loadCategoryList()
+  }
+  //加载一级品类
+  loadCategoryList(){
+    _product.getCategoryList().then(res => {
+     
+      this.setState({
+        firstCategoryList: res
+      })
+    },errMsg => {
+      _mm.errorTips(errMsg)
+    })
+  }
+  //选择一级品类
+  handleFirstCategoryList(e){
+    let newValue = e.target.value
+    this.setState({
+      //选中一级品类，并清空二级品类
+      firstCategoryId: newValue,
+      secondCategoryList: [],
+      secondCategoryId: 0
+    },//加载二级品类
+    () => {
+      this.loadSecondCategory()
+      this.onPropsCategroyChange()
+    })
+  }
+  //加载二级品类
+  loadSecondCategory(){
+     _product.getCategoryList(this.state.firstCategoryId).then(res => {
+      
+      this.setState({
+        secondCategoryList: res
+      })
+    },errMsg => {
+      _mm.errorTips(errMsg)
+    })
+  } 
+  //二级品类的change 事件
+  handleSecondCategoryList(e){
+    let newValue = e.target.value
+    this.setState({
+      secondCategoryId: newValue
+    },//加载二级品类
+    () => {
+      this.onPropsCategroyChange()
+    })
+  } 
+ //传给父组件中的结果
+  onPropsCategroyChange(){
+    let category = typeof this.props.onCategoryChange === "function"
+    if(this.state.secondCategoryId){
+      category && this.props.onCategoryChange(this.state.secondCategoryId,this.state.firstCategoryId)
+    }else{
+      category && this.props.onCategoryChange(this.state.firstCategoryId,0)
+    }
+  }
+  render(){
+   
+    return (
+       <div className="col-md-10">
+        <select className="form-control cate-select"
+        onChange = {(e) => {this.handleFirstCategoryList(e)}}
+        >
+          <option value = "" className = "cate-option">请选择一级分类</option>
+          {
+            this.state.firstCategoryList.map(
+              (category,index)=>
+                <option key = {index} value = {category.id} className = "cate-option">{category.name}</option>
+            )
+          }
+        </select>
+        {
+          this.state.secondCategoryList.length ? 
+             (<select name = "" className="form-control cate-select"
+             onChange = {(e) => {this.handleSecondCategoryList(e)}}
+             >
+                <option value = "">请选择二级分类</option>
+                  {
+                    this.state.secondCategoryList.map(
+                      (category,index)=>
+                        <option key = {index} value = {category.id} className = "cate-option">{category.name}</option>
+                    )
+                  }
+            </select>)
+          : null
+        }
+        
+      </div>
+    )
+  }
+}
+export default CategorySelector
---
+++ src/page/product/index/save.jsx
@@ -0,0 +1,83 @@
+import React,{Component} from "react"
+import PageTitle from "component/page-title/index.jsx"
+import Product from "service/product-service.jsx"
+import Util from "util/mm.jsx"
+import CategorySelector from "./category-selector.jsx"
+const _mm = new Util()
+const _product = new Product()
+class ProductSave extends Component {
+  constructor(props){
+    super(props),
+    this.state = {
+      categoryId: 0,
+      parentCategoryId: 0
+    }
+    this.handleCateGoryChange = this.handleCateGoryChange.bind(this)
+  }
+  handleCateGoryChange(categoryId,parentCategoryId){
+    console.log(categoryId,parentCategoryId)
+  }
+  render(){
+    return(
+      <div id = "page-wrapper">
+        <PageTitle title = "添加商品" />
+          <form className="form-horizontal">
+            <div className="form-group">
+             <label className="col-md-2 control-label">商品名称</label>
+              <div className="col-md-5">
+                <input type="text" className="form-control" placeholder="请输入商品名称"/>
+              </div>
+            </div>
+            <div className="form-group">
+              <label className="col-md-2 control-label">商品描述</label>
+              <div className="col-md-5">
+                <input type="text" className="form-control" placeholder="请输入商品描述"/>
+              </div>
+            </div>
+            <div className="form-group">
+              <label className="col-md-2 control-label">所属分类</label>
+              <CategorySelector onCategoryChange = {
+                 (categoryId,parentCategoryId) => {this.handleCateGoryChange(null,categoryId,parentCategoryId)}}
+                 />
+            </div>
+            <div className="form-group">
+              <label className="col-md-2 control-label">商品价格</label>
+              <div className="col-md-3">
+                <div className="input-group">
+                  <input type="number" className="form-control" placeholder="请输入商品价格"/>
+                  <span className="input-group-addon" >元</span>
+                </div> 
+              </div>
+            </div>
+            <div className="form-group">
+              <label className="col-md-2 control-label">商品库存</label>
+              <div className="col-md-3">
+                <div className="input-group">
+                    <input type="number" className="form-control" placeholder="库存"/>
+                    <span className="input-group-addon" >件</span>
+                </div>
+              </div>
+            </div>
+            <div className="form-group">
+              <label className="col-md-2 control-label">商品图片</label>
+              <div className="col-md-10">
+                xxxx
+              </div>
+            </div>
+            <div className="form-group">
+              <label className="col-md-2 control-label">商品详情</label>
+              <div className="col-md-10">
+                detail
+              </div>
+            </div>
+            <div className="form-group">
+              <div className="col-sm-offset-2 col-sm-10">
+                <button type="submit" className="btn btn-primary">提交</button>
+              </div>
+            </div>
+        </form>
+      </div>
+      )
+  }
+}
+export default ProductSave
